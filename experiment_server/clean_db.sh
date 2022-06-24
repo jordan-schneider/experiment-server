105f710cb0a73a89d63629e437740bf2f0818404
Jordan Schneider <jordan.jack.schneider@gmail.com>
2022-06-24 17:44:13 -0500

    Fixes typo in clean_db.sh

--- experiment_server/clean_db.sh
+++ experiment_server/clean_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm experiments.db
-python ~/web-procgen/quesiton-gen/question_gen/gen_questions.py init-db experiments.db schema.sql
-python ~/web-procgen/quesiton-gen/question_gen/gen_questions.py gen-random-questions experiments.db miner 10 traj
+python ~/web-procgen/question-gen/question_gen/gen_questions.py init-db experiments.db schema.sql
+python ~/web-procgen/question-gen/question_gen/gen_questions.py gen-random-questions experiments.db miner 10 traj
