e6c84b665f3098d378a7125eb9e1f20d0f82f085
Jordan Schneider <jordan.jack.schneider@gmail.com>
2022-06-24 18:02:20 -0500

    Move node configs to package root.

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: {
+        interact: { import: "./experiment_server/js/interact.js", filename: "[name].js" },
+        record: { import: "./experiment_server/js/record.js", filename: "[name].js" },
+        replay: { import: "./experiment_server/js/replay.js", filename: "[name].js" },
+        yuri: { import: "./experiment_server/js/yuri_demo.js", filename: "[name].js" },
+
+    },
+    output: {
+        path: path.resolve(__dirname, 'experiment_server/static'),
+    },
+};
