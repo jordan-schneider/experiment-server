5a6c7110226f7e6b42443ecb881617301c69749a
Jordan Schneider <jordan.jack.schneider@gmail.com>
2022-09-09 13:22:33 -0400

    Utility script to download experiment db from s3.

---
+++ pull-s3-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install awscli if needed
+if ! [ -x "$(command -v aws)" ]; then
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  ./aws/install --install-dir ~/.local/aws-cli --bin-dir ~/.local/bin
+fi
+
+aws s3 cp  s3://mrl-experiment-sqlite/experiments.db experiment_server/experiments.db
